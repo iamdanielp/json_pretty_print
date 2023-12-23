087c866e90d1b0a22caaf396ce4b3e5552fa8b1d
Daniel Persson <daniel.persson@skolon.com>
2023-12-23 21:22:13 +0100

    - Removed unused dependencies. - Added some logging.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 use std::collections::HashMap;
 use std::fs;
 use clap::Parser;
-use std::fs::File;
-use std::io::{Read, Write};
-use serde::{Deserialize, Serialize};
 use serde_json::Value;
-
-#[derive(Serialize, Deserialize, Debug)]
-struct U;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -22,13 +16,15 @@ struct Args {
 fn main() -> std::io::Result<()> {
     let args: Args = Args::parse();
 
+    println!("Reading {}...", args.file);
+
     let data = fs::read_to_string(args.file).expect("Unable to open input file.");
     let json: HashMap<String, Value> = serde_json::from_str(&data).unwrap();
     let pretty = serde_json::to_string_pretty(&json).unwrap();
 
-    println!("{:?}", pretty);
+    fs::write(&args.out, pretty).expect("Unable to write to output file.");
 
-    fs::write(args.out, pretty).expect("Unable to write to output file.");
+    println!("Successfully wrote file {}", &args.out);
 
     return Ok(());
 }
